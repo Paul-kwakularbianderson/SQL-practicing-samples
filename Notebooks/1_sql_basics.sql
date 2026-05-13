79927f4353355a41ade79de9baa3bfc3582bf5d8
Paul Kwaku Larbi Anderson <273589303+Pakanderson@users.noreply.github.com>
2026-05-13 22:43:47 +0200

    Update 1_sql_basics.sql

--- Notebooks/1_sql_basics.sql
+++ Notebooks/1_sql_basics.sql
@@ -7,13 +7,13 @@ SQL CHALLENGES 1
 *******************************************************************************
 *******************************************************************************
 
-In the exercises below you will need to use the following clauses:
+In the exercises below, you will need to use the following clauses:
 	- SELECT FROM
     - AS
 	- DISTINCT
 
-In SQL we can have many databases, they will show up in the schemas list
-We must first define which database we will be working with.
+In SQL, we can have many databases; they will show up in the schemas list
+We must first determine which database we will work with.
 */
 
 USE publications;
@@ -28,9 +28,9 @@ https://www.w3schools.com/sql/sql_select.asp
 SELECT * FROM authors;
 
 
-/* 2. Select everything from the tables "titles", "titleauthor", "publishers"
+/* 2. Select everything from the tables "titles", "titleauthor", and "publishers."
    and "sales" (one table at a time). Look at the raw data and think about how 
-   these tables are related to each other. */
+   These tables are related to each other. */
 SELECT * FROM titles;
 
 SELECT * FROM titleauthor;
@@ -39,7 +39,7 @@ SELECT * FROM publishers;
 
 SELECT * FROM sales;
 
--- 3. Select the authors first and last name
+-- 3. Select the author's first and last name
 SELECT 
     au_fname, au_lname
 FROM
@@ -51,7 +51,7 @@ SELECT
 FROM
     publishers;
 
--- 5. Select the title, price and year-to-date sales of all titles
+-- 5. Select the title, price, and year-to-date sales of all titles
 SELECT 
     title, price, ytd_sales
 FROM
@@ -64,7 +64,7 @@ AS
 https://www.w3schools.com/sql/sql_ref_as.asp
 ******************************************************************************/
 
-/* 6. Select all the authors first names, but display the column with the
+/* 6. Select all the authors' first names, but display the column with the
    alias "first_name" */
 SELECT 
 	au_fname AS first_name
